5a4ee80e7682c69634396c221e644d0b6bac1151
Crowdbotics <engineering@crowdbotics.com>
2023-06-25 01:27:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-fire-dev-94486",
+  url: "https://odd-fire-dev-94486.botics.co",
+  api: "https://odd-fire-dev-94486.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
